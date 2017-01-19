b5186b2d16ba1eadcd950e6c6ca62b112fedb93b
Thomas Tracey <thomas@tracey.com>
2017-01-18 17:52:48 -0800

    solution for counting 'persons of interest' (POI)

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,28 @@
 """
 
 import pickle
+from collections import Counter
+
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+# number of people in the file
+print "people:", len(enron_data)
 
+# number of attributes for each person
+print "features:", len(enron_data.itervalues().next())
+
+# total number of datapoints
+print "values:", sum(len(v) for v in enron_data.itervalues())
+
+# count of 'persons of interest' (POI)
+def count_poi(d):
+	count = 0
+	for k, v in d.iteritems():
+		if isinstance(v, dict):
+			for l, u in v.iteritems():
+				if l == 'poi' and u == True:
+					count += 1
+	print "poi count:", count
+
+count_poi(enron_data)
